df6a41ab067f91eb703a8288aebcf2c479bf8b95
kavingithub7 <145750303+kavingithub7@users.noreply.github.com>
2023-11-09 14:57:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Home from './Home';
+
+
+
+
+function App() {
+  return (
+   
+        <Home />
+        
+        
+        
+        
+        
+    
+  );
+}
+
+export default App;
